678ee8cd2f4d1e06a1bdc0c0f50755868baff84b
lopic456 <kiriki.kamori@yandex.ru>
2026-06-01 08:36:40 +0300

    Добавлен класс CaesarCipher с методами шифрования и расшифровки

--- java/CaesarCipher.java
+++ java/CaesarCipher.java
@@ -1,2 +1,21 @@
 public class CaesarCipher {
+
+    public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        shift = shift % 26;
+
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                result.append((char) ((c - base + shift) % 26 + base));
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String text, int shift) {
+        return encrypt(text, 26 - (shift % 26));
+    }
 }
