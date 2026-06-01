4a7b5f1cfc923c7481a623558297bab9f1045978
lopic456 <kiriki.kamori@yandex.ru>
2026-06-01 08:37:00 +0300

    Добавлен FileWriter для сохранения результата в файл

--- java/FileWriter.java
+++ java/FileWriter.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileWriter {
+
+    public static void writeFile(String content, String filePath) throws IOException {
+        Path path = Path.of(filePath);
+        Files.writeString(path, content);
+        System.out.println("✅ Файл успешно сохранён: " + filePath);
+    }
 }
